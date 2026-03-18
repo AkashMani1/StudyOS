211e730c31597612fe748bf12efaba9cd7fb746d
AkashMani1 <akashmani9955@gmail.com>
2026-03-18 22:32:24 +0530

    Lazy-load Firebase Admin for build-safe server routes

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,7 +1,7 @@
 import { getApps, initializeApp, cert, type App } from "firebase-admin/app";
-import { getAuth } from "firebase-admin/auth";
-import { getFirestore } from "firebase-admin/firestore";
-import { getMessaging } from "firebase-admin/messaging";
+import { getAuth, type Auth } from "firebase-admin/auth";
+import { getFirestore, type Firestore } from "firebase-admin/firestore";
+import { getMessaging, type Messaging } from "firebase-admin/messaging";
 import { getServerEnv } from "@/lib/env";
 import { HttpError } from "@/lib/server-errors";
 
@@ -30,8 +30,14 @@ function createAdminApp(): App {
   });
 }
 
-const adminApp = createAdminApp();
+function createLazyProxy<T extends object>(factory: () => T): T {
+  return new Proxy({} as T, {
+    get(_target, property, receiver) {
+      return Reflect.get(factory(), property, receiver);
+    }
+  });
+}
 
-export const adminAuth = getAuth(adminApp);
-export const adminDb = getFirestore(adminApp);
-export const adminMessaging = getMessaging(adminApp);
+export const adminAuth: Auth = createLazyProxy(() => getAuth(createAdminApp()));
+export const adminDb: Firestore = createLazyProxy(() => getFirestore(createAdminApp()));
+export const adminMessaging: Messaging = createLazyProxy(() => getMessaging(createAdminApp()));
